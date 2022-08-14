8c87527b107ff91004385e80876c8d9c83493372
IoniBini <ionimaghidman@gmail.com>
2022-08-15 00:15:08 +1000

    Started Terrain generation
    Needs fixes, need to ask questions:
    
    - neighboring system not working yet
    - how do I clamp values in perlin noise to create less and more noisy areas
    - need to change colors

---
+++ Procedural Aqua Caves/Assets/Scripts/ChildTerrainGeneration.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildTerrainGeneration : MonoBehaviour
+{
+    private int terrainResolutionChild = 1;
+
+    private int widthChild; //x-axis of the terrain
+    private int heightChild; //z-axis
+
+    private int depthChild; //y-axis
+
+    private float scaleChild;
+
+    [SerializeField] private float offsetXChild;
+    [SerializeField] private float offsetYChild;
+
+    public TerrainGeneration parentData;
+
+    public void ChildGenerateTerrain()
+    {
+        //the first step is to collect all the data present in the parent and store it in each individual child obj
+        parentData = transform.parent.GetComponent<TerrainGeneration>();
+
+        terrainResolutionChild = parentData.terrainResolution;
+        widthChild = parentData.width;
+        heightChild = parentData.height;
+        depthChild = parentData.depth;
+        scaleChild = parentData.scale;
+        offsetXChild = parentData.offsetX;
+        offsetYChild = parentData.offsetY;
+                       
+        //terrain.SetNeighbors(null , null, null, null);
+
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+    }
+
+    TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = widthChild + 1;
+        terrainData.size = new Vector3(widthChild, depthChild, heightChild);
+
+        terrainData.SetHeights(0, 0, GenerateHeights());
+        return terrainData;
+    }
+
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[widthChild, heightChild];
+        for (int x = 0; x < widthChild; x++)
+        {
+            for (int y = 0; y < heightChild; y++)
+            {
+                heights[x, y] = CalculateHeight(x, y);
+            }
+        }
+
+        return heights;
+    }
+
+    float CalculateHeight(int x, int y)
+    {
+        float xCoord = (float)x / widthChild * scaleChild + offsetXChild;
+        float yCoord = (float)y / heightChild * scaleChild + offsetYChild;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
---
+++ Procedural Aqua Caves/Assets/Scripts/TerrainGeneration.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGeneration : MonoBehaviour
+{
+    //this script is inspired by brackeys: https://www.youtube.com/watch?v=vFvwyu_ZKfU
+    //I didnt want to just copy sebastian lagues whole landmass generator video so instead, I will ATTEMPT to take a different approach, where I will mix elements from
+    //Brackeys, Sebastian and Lachlans codes so that I turn out with something slightly more original of my part (if "frankensteining" code can be called original)
+    //Im doing this not because it will be more efficient, it is exclusively because I want to show that I do know how to code as opposed to just copying
+
+    //to avoid causing errors due to the resolution of the terrain not being in multiples of the power of 2, I have created these variables to multiply with
+    //I have limited it to 7 arbitrarily, I just think that after 4, the performance gets too janky to be acceptable
+    [Range(1, 7)] public int terrainResolution = 1;
+    public int terrainRows = 1;
+    public int terrainCollumns = 1;
+
+    //I dont want these to be changed, so I'll hide them from view
+    [HideInInspector] public int width = 32; //x-axis of the terrain
+    [HideInInspector] public int height = 32; //z-axis
+
+    public int depth = 20; //y-axis
+
+    public float scale = 20f;
+
+    public float offsetX = 100f;// equivalent to the width
+    public float offsetY = 100f;// equivalent to the height
+
+    public GameObject prefab;
+
+    public Terrain left;
+    public Terrain top;
+    public Terrain right;
+    public Terrain bottom;
+
+    [ContextMenu("GenerateTerrain")]
+    private void ParentGenerateTerrain()
+    {
+        //delete the previous stuff prior to commencing
+        ClearTerrain();
+
+        //reset the x and y values because they might have been modified by the resolution
+        width = 32;
+        height = 32;
+
+        //pick a new offset to make sure its random each time
+        offsetX = Random.Range(0f, 9999f);
+        offsetY = Random.Range(0f, 9999f);
+
+        for (int i = 0; i <= terrainResolution - 1; i++)
+        {
+            width *= 2;
+            height *= 2;
+        }
+
+        for (int i = 0; i <= terrainRows - 1; i++)
+        {
+            //stores and reasigns the offsetX so that it doesnt keep on infinitely adding after beign done with one row and going up to the next one
+            var tempOffsetX = offsetX;
+
+            //the minus 1 is due to the fact that the parent terrain occupies one of the slots
+            for (int j = 0; j <= terrainCollumns - 1; j++)
+            {
+                //gotta fix the placement, it doesnt take into account the resolution variable
+                var newTerrain = Instantiate(prefab, new Vector3(transform.position.x + width * j, transform.position.y, transform.position.z + width * i), transform.rotation);//.GetComponent<ChildTerrainGeneration>();
+                newTerrain.name = "X" + j + "Y" + i;
+                newTerrain.gameObject.transform.parent = transform;
+
+                //use the names to determine who is going to be a neighbour to the current terrain (not currently working...)
+                Debug.Log((j - 1).ToString() + i.ToString());
+                if (GameObject.Find((j - 1).ToString() + i.ToString()) != null)
+                {
+                    left = GameObject.Find((j - 1).ToString() + i.ToString()).GetComponent<Terrain>(); 
+                }
+                else
+                {
+                    left = null;
+                }
+
+
+                if (GameObject.Find(j.ToString() + (i + 1).ToString()) != null)
+                {
+                    top = GameObject.Find((j - 1).ToString() + i.ToString()).GetComponent<Terrain>();
+                }
+                else
+                {
+                    top = null;
+                }
+
+
+                if (GameObject.Find((j + 1).ToString() + i.ToString()) != null)
+                {
+                    right = GameObject.Find((j - 1).ToString() + i.ToString()).GetComponent<Terrain>();
+                }
+                else
+                {
+                    right = null;
+                }
+
+
+                if (GameObject.Find(j.ToString() + (i - 1).ToString()) != null)
+                {
+                    bottom = GameObject.Find((j - 1).ToString() + i.ToString()).GetComponent<Terrain>();
+                }
+                else
+                {
+                    bottom = null;
+                }
+
+                newTerrain.GetComponent<Terrain>().SetNeighbors(left, top, right, bottom);
+
+                newTerrain.GetComponent<ChildTerrainGeneration>().ChildGenerateTerrain();
+
+                offsetX += height;
+            }
+
+            offsetY += width;
+
+            offsetX = tempOffsetX;
+        }
+    }
+
+    [ContextMenu("ClearTerrain")]
+    private void ClearTerrain()
+    {
+        if (transform.childCount != 0)
+        {
+            //We iterate backwards through children, since if we go forwards we end up changing child indices
+            for (var i = transform.childCount - 1; i >= 0; i--)
+            {
+                //DestroyImmediate is for edit-mode, Destroy is for play-mode (including in the built application)
+                if (Application.isPlaying) Destroy(transform.GetChild(i).gameObject);
+                else DestroyImmediate(transform.GetChild(i).gameObject);
+            }
+        }
+    }
+}
